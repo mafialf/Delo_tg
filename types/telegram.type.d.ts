2022dfc1e54ff7f892df39ad7c9b47ee87a7fc10
mafialf <Lobanov.mafia@yandex.ru>
2025-06-26 22:00:48 +0500

    Add files via upload

---
+++ types/telegram.type.d.ts
@@ -0,0 +1,22 @@
+// types/telegram.type.d.ts
+
+declare namespace window {
+  const Telegram: {
+    WebApp: {
+      initDataUnsafe: {
+        user: {
+          id: number;
+          username?: string;
+        };
+      };
+      openLink: (url: string) => void;
+      backButton: {
+        show: () => void;
+        hide: () => void;
+        onClick: (callback: () => void) => void;
+        offClick: (callback: () => void) => void;
+      };
+      close: () => void;
+    };
+  };
+}
